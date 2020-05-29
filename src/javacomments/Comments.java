cf7bd23e417568cd6bd62939d9bc1ca0a3d46984
luongngodac <55689857+luongngodac@users.noreply.github.com>
2020-05-29 10:37:50 +0700

    this is comments in Java

---
+++ src/javacomments/Comments.java
@@ -0,0 +1,12 @@
+package javacomments;
+
+public class Comments {
+    public static void main(String[] args) {
+        //This is Single-line comment before a line of code
+        System.out.println("Comments advance."); // This is Single-line at the end of line of code.
+        /*
+        This is multi-comment.
+        */
+    }
+    
+}
