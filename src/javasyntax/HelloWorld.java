da005f6a75f5aeecf0e21670aafe4ca807bdf499
luongngodac <55689857+luongngodac@users.noreply.github.com>
2020-05-29 10:31:44 +0700

    fisrt progam: Hello World

---
+++ src/javasyntax/HelloWorld.java
@@ -0,0 +1,8 @@
+package javasyntax;
+public class HelloWorld{
+    //Create a simple "Hellow World" program.
+    public static void main(String[] args) {
+        System.out.println ("Hello World! Java Study");
+    }
+    
+}
